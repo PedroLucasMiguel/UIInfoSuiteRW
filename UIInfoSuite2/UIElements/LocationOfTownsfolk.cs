f00a0a948fcd790b367f156baeedfdf3ca23ddc3
Pedro Lucas Miguel <pedro.lu.miguel@gmail.com>
2024-07-25 08:22:36 -0300

    Removing unnecessary import

--- UIInfoSuite2/UIElements/LocationOfTownsfolk.cs
+++ UIInfoSuite2/UIElements/LocationOfTownsfolk.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
